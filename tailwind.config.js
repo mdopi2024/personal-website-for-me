aa1ab7d43843c3ec22255382cdd9610b451eb56a
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2024-08-25 16:58:25 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    fontFamily:{
+      "font": ['"Manrope", sans-serif']
+    },
+    extend: {
+      colors:{
+        'primary-cl': "FD6F00",
+      },
+    },
+  },
+  plugins: [],
+}
+
